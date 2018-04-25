27cd5fc84e426f7efa59ae2624c4bacc76c02352
BRobat <Patogeen@icloud.com>
2018-04-25 23:19:08 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Project1Component } from './project1/project1.component';
+import { Project2Component } from './project2/project2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Project1Component,
+    Project2Component
   ],
   imports: [
     BrowserModule
