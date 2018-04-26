4534edc6ea1a184a0f95fe291f7de3cf8be441ef
BRobat <Patogeen@icloud.com>
2018-04-26 11:48:40 +0200

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Project1Component } from './project1/project1.component';
 import { Project2Component } from './project2/project2.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 @NgModule({
@@ -13,10 +16,18 @@ import { Project2Component } from './project2/project2.component';
     AppComponent,
     NavbarComponent,
     Project1Component,
-    Project2Component
+    Project2Component,
+    HomePageComponent,
+    ContactPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomePageComponent},
+      {path: 'project1', component: Project1Component},
+      {path: 'project2', component: Project2Component},
+      {path: 'contact', component: ContactPageComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
